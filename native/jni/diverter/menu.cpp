2a0c739f17f3fc3f1865d998d85535f9e20063e1
Peter Cai <peter@typeblog.net>
2021-05-04 14:28:34 +0800

    menu: extract logic for cached menu items

--- native/jni/diverter/menu.cpp
+++ native/jni/diverter/menu.cpp
@@ -1,18 +1,21 @@
 #include "menu.h"
 #include "main.h"
 
-MainMenu::MainMenu() {
-    items = make_shared<vector<MenuItem>>();
-    items->push_back(MenuItem(ACTION_BOOT_INTERNAL, "Boot from internal storage"));
-    items->push_back(MenuItem(ACTION_BOOT_SDCARD, "Boot from external SD card"));
+shared_ptr<vector<MenuItem>> CachedMenu::getItems() {
+    if (items == nullptr) {
+        items = make_shared<vector<MenuItem>>();
+        populateItems();
+    }
+    return items;
 }
 
 string MainMenu::getTitle() {
     return "Boot Diverter";
 }
 
-shared_ptr<vector<MenuItem>> MainMenu::getItems() {
-    return items;
+void MainMenu::populateItems() {
+    items->push_back(MenuItem(ACTION_BOOT_INTERNAL, "Boot from internal storage"));
+    items->push_back(MenuItem(ACTION_BOOT_SDCARD, "Boot from external SD card"));
 }
 
 void MainMenu::onItemSelected(int action) {
--- native/jni/diverter/menu.h
+++ native/jni/diverter/menu.h
@@ -4,14 +4,20 @@
 using namespace std;
 using namespace UI;
 
+class CachedMenu : public Menu {
+protected:
+    shared_ptr<vector<MenuItem>> items;
+    virtual void populateItems() = 0;
+public:
+    shared_ptr<vector<MenuItem>> getItems();
+};
+
 #define ACTION_BOOT_INTERNAL 1001
 #define ACTION_BOOT_SDCARD   1002
-class MainMenu : public Menu {
-private:
-    shared_ptr<vector<MenuItem>> items;
+class MainMenu : public CachedMenu {
 public:
-    MainMenu();
+    MainMenu() = default;
     string getTitle();
-    shared_ptr<vector<MenuItem>> getItems();
+    void populateItems();
     void onItemSelected(int action);
 };
