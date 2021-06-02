aa894a7e83b2e4a9431bde39085e37cdabb00e30
Peter Cai <peter@typeblog.net>
2021-06-02 17:02:09 +0800

    inject: entry: preserve original LD_PRELOAD if any

--- native/inject/jni/common/entry.cpp
+++ native/inject/jni/common/entry.cpp
@@ -24,6 +24,14 @@ int main(int argc, char *argv[]) {
     string real_binary = "/debug_ramdisk/dev/mntRAM/bin/" + progname;
     LOGI("Loading LD_PRELOAD hook: %s", hook_lib.c_str());
     LOGI("Loading original binary: %s", real_binary.c_str());
+    
+    char *_preload_orig;
+    if ((_preload_orig = getenv("LD_PRELOAD")) != nullptr) {
+        // Preserve the original LD_PRELOAD if any
+        // This may be used sometimes on some ROMs or in mods
+        hook_lib = hook_lib + ":" + _preload_orig;
+    }
+    
     setenv("LD_PRELOAD", hook_lib.c_str(), 1);
     execve(real_binary.c_str(), argv, environ);
     return errno;
